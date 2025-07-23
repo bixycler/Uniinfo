b897a4110652df77978ac68853dffcff00eea498
Will LE <lexuandinhct@gmail.com>
2025-07-23 20:01:22 +0700

    Fix spelling; Add google-site-verification: google271c70da1dda0983.html

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -2,7 +2,7 @@
     //this.classList.toggle("active");
     if(typeof(obj)=='string'){ obj = document.getElementsById(obj); }
     if (getComputedStyle(obj, null).getPropertyValue("display") === "none") {
-        obj.style.display = "initial";
+        obj.style.display = "revert";
     } else {
         obj.style.display = "none";
     }
